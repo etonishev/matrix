a626e216fd24ea2f01d0a1ddae9265f680432d46
Eugene G. Tonishev <tonisheg@yandex.ru>
2022-10-17 15:31:36 +0400

    Rewrite with using std::vector

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -1,234 +1,133 @@
 #ifndef MATRIX_INCLUDE_MATRIX_HPP_
 #define MATRIX_INCLUDE_MATRIX_HPP_
 
-#include <cstddef>
+#include <vector>
 
 
 
 namespace teg {
 
     template<typename T>
+    using Row = std::vector<T>;
+
+    template<typename T>
     class Matrix {
-
-    public:
-        class Row {
-        
         public:
-            Row() = delete;
-            Row(T* row);
+            Matrix();
+            Matrix(std::size_t size);
+            Matrix(std::size_t rows, std::size_t cols);
+            Matrix(std::size_t size, const T& value);
+            Matrix(std::size_t rows, std::size_t cols, const T& value);
 
-            ~Row() = default;
+            Matrix(const Matrix& other);
+            Matrix(Matrix&& other) noexcept;
 
-            T& operator[](std::size_t index);
-            const T& operator[](std::size_t index) const;
+            ~Matrix();
+
+            std::size_t cols() const noexcept;
+            std::size_t rows() const noexcept;
+
+            bool empty() const noexcept;
+
+            Row<T>& operator[](std::size_t index);
+            const Row<T>& operator[](std::size_t index) const;
+
+            Matrix& operator=(const Matrix& other);
+            Matrix& operator=(Matrix&& other) noexcept;
 
         private:
-            T* _row;
-
-        }; // !class Row
-
-    public:
-        Matrix();
-        Matrix(std::size_t size);
-        Matrix(std::size_t r, std::size_t c);
-        Matrix(std::size_t size, const T& value);
-        Matrix(std::size_t r, std::size_t c, const T& value);
-
-        Matrix(const Matrix& that);
-        Matrix(Matrix&& that) noexcept;
-
-        ~Matrix();
-
-        std::size_t rows() const noexcept;
-        std::size_t cols() const noexcept;
-
-        bool empty() const noexcept;
-
-        Row& operator[](std::size_t row);
-        const Row& operator[](std::size_t row) const;
-
-        Matrix& operator=(const Matrix& that);
-        Matrix& operator=(Matrix&& that) noexcept;
-
-    private:
-        void _clear();
-
-    private:
-        int** _storage;
-        std::size_t _rows;
-        std::size_t _cols;
-
-    }; // !class Matrix
+            std::vector<Row<T>> m_storage;
+    }; // !class Matrix<T>
 
     template<typename T>
     Matrix<T>::Matrix() :
-        _storage(nullptr), _rows(0), _cols(0) { }
+        m_storage() {}
 
     template<typename T>
     Matrix<T>::Matrix(std::size_t size) :
-        Matrix(size, size, 0) { }
+        Matrix(size, size) {}
 
     template<typename T>
-    Matrix<T>::Matrix(std::size_t r, std::size_t c) :
-        Matrix(r, c, 0) { }
+    Matrix<T>::Matrix(std::size_t rows, std::size_t cols) :
+        Matrix(rows, cols, {}) {}
 
     template<typename T>
     Matrix<T>::Matrix(std::size_t size, const T& value) :
-        Matrix(size, size, value) { }
+        Matrix(size, size, value) {}
 
     template<typename T>
-    Matrix<T>::Matrix(std::size_t r, std::size_t c, const T& value) :
-        _storage(new T*[r]), _rows(r), _cols(c) {
+    Matrix<T>::Matrix(std::size_t rows, std::size_t cols, const T& value) :
+        m_storage(rows, std::vector<T>(cols, value)) {}
 
-        for (std::size_t i = 0; i < rows(); ++i) {
-
-            _storage[i] = new T[cols()];
-
-            for (std::size_t j = 0; j < cols(); ++j)
-                _storage[i][j] = value;
-
-        }
-
+    template<typename T>
+    Matrix<T>::Matrix(const Matrix<T>& other) :
+        m_storage(other.m_storage) {
     }
 
     template<typename T>
-    Matrix<T>::Matrix(const Matrix<T>& that) :
-        _storage(new T*[that.rows()]), _rows(that.rows()), _cols(that.cols()) {
-
-        for (std::size_t i = 0; i < rows(); ++i) {
-
-            _storage[i] = new T[cols()];
-
-            for (std::size_t j = 0; j < cols(); ++j)
-                _storage[i][j] = that[i][j];
-
-        }
-
-    }
+    Matrix<T>::Matrix(Matrix<T>&& other) noexcept :
+        m_storage(std::move(other.m_storage)) {}
 
     template<typename T>
-    Matrix<T>::Matrix(Matrix<T>&& that) noexcept :
-        _storage(that._storage), _rows(that.rows()), _cols(that.cols()) {
-
-        that._storage = nullptr;
-        that._rows = 0;
-        that._cols = 0;
-
-    }
-
-    template<typename T>
-    Matrix<T>::~Matrix() {
-
-        _clear();
-
-    }
+    Matrix<T>::~Matrix() {}
 
     template<typename T>
     std::size_t Matrix<T>::rows() const noexcept {
 
-        return _rows;
+        return m_storage.size();
 
     }
 
     template<typename T>
-    size_t Matrix<T>::cols() const noexcept {
+    std::size_t Matrix<T>::cols() const noexcept {
 
-        return _cols;
+        if (m_storage.empty()) return 0;
+        return m_storage[0].size();
 
     }
 
     template<typename T>
     bool Matrix<T>::empty() const noexcept {
 
-        return _storage == nullptr;
+        return m_storage.empty();
 
     }
 
     template<typename T>
-    typename Matrix<T>::Row& Matrix<T>::operator[](std::size_t row) {
+    Row<T>& Matrix<T>::operator[](std::size_t index) {
 
-        return Row(_storage[row]);
-
-    }
-
-    template<typename T>
-    typename const Matrix<T>::Row& Matrix<T>::operator[](std::size_t row) const {
-
-        return Row(_storage[row]);
+        return const_cast<Row<T>&>(static_cast<const Matrix&>(*this)[index]);
 
     }
 
     template<typename T>
-    Matrix<T>& Matrix<T>::operator=(const Matrix<T>& that) {
+    const Row<T>& Matrix<T>::operator[](std::size_t index) const {
 
-        if (this == &that)
+        return m_storage[index];
+
+    }
+
+    template<typename T>
+    Matrix<T>& Matrix<T>::operator=(const Matrix<T>& other) {
+
+        if (this == &other)
             return *this;
 
-        _clear();
-
-        _rows = that.rows();
-        _cols = that.cols();
-
-        _storage = new T*[rows()];
-
-        for (std::size_t i = 0; i < rows(); ++i) {
-
-            _storage[i] = new T[cols()];
-
-            for (std::size_t j = 0; j < cols(); ++j)
-                _storage[i][j] = that[i][j];
-
-        }
+        m_storage = other.m_storage;
 
         return *this;
 
     }
 
     template<typename T>
-    Matrix<T>& Matrix<T>::operator=(Matrix<T>&& that) noexcept {
+    Matrix<T>& Matrix<T>::operator=(Matrix<T>&& other) noexcept {
 
-        if (this == &that)
+        if (this == &other)
             return *this;
 
-        _clear();
-
-        std::swap(_storage, that._storage);
-        std::swap(_rows, that._rows);
-        std::swap(_cols, that._cols);
+        m_storage = std::move(other.m_storage);
 
         return *this;
-
-    }
-
-    template<typename T>
-    void Matrix<T>::_clear() {
-
-        for (std::size_t i = 0; i < rows(); ++i)
-            delete[] _storage[i];
-
-        delete[] _storage;
-
-        _storage = nullptr;
-        _rows = 0;
-        _cols = 0;
-
-    }
-
-    template<typename T>
-    Matrix<T>::Row::Row(T* row) :
-        _row(row) { }
-
-    template<typename T>
-    T& Matrix<T>::Row::operator[](std::size_t index) {
-
-        return _row[index];
-
-    }
-
-    template<typename T>
-    const T& Matrix<T>::Row::operator[](std::size_t index) const {
-
-        return _row[index];
 
     }
 
--- main.cpp
+++ main.cpp
@@ -6,19 +6,26 @@
 
 int main() {
 
-    teg::Matrix<int> matrix(3, 3);
+    teg::Matrix<int> m1 {};             // empty matrix
+    teg::Matrix<int> m2 {3};            // matrix 3 x 3
+    teg::Matrix<int> m3 {4, 4};         // matrix 4 x 4
+    teg::Matrix<int> m4 {5, 10};        // matrix 5 x 5 with default value is 10
+    teg::Matrix<int> m5 {6, 6, 15};     // matrix 6 x 6 with default value is 15
 
-    matrix[1][1] = 7;
+    teg::Matrix<int> copyMatrix { m5 };
+    teg::Matrix<int> moveMatrix { std::move(copyMatrix) };
 
-    for (std::size_t i = 0; i < matrix.rows(); ++i) {
+    std::cout << "There are " << moveMatrix.rows() << " rows and "
+        << moveMatrix.cols() << " cols in the moveMatrix\n";
 
-        for (std::size_t j = 0; j < matrix.cols(); ++j)
-            std::cout << matrix[i][j] << ' ';
+    std::cout << "Is this empty? " << std::boolalpha << moveMatrix.empty() << '\n';
 
-        std::cout << '\n';
+    std::cout << moveMatrix[0][0] << '\n';
+    moveMatrix[0][0] = -15;
+    std::cout << moveMatrix[0][0] << '\n';
 
-    }
-
+    m1 = moveMatrix;
+    m1 = std::move(moveMatrix);
 
     return 0;
 
